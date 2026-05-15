03623acfe82b829c3cfabc097cb97c110f404dbc
mazentry <mazencode10@gmail.com>
2026-05-15 20:04:35 +0300

    add cors

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ require("dotenv").config()
 const express = require("express")
 const axios = require("axios")
 const OpenAI = require("openai")
+const cors = require("cors")
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const processedMessages = new Set()
 
