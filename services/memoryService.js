1660da1cfb308ba83d6242be6a3a9cf486bd9e3f
pioneercloudtechnologies <pioneercloud.technologies@gmail.com>
2026-07-06 15:40:52 +0300

    update duplicated memory

--- services/memoryService.js
+++ services/memoryService.js
@@ -6,17 +6,41 @@ const saveMemory = async (
   value
 ) => {
 
-  const { data, error } = await supabase
-    .from("memories")
-    .insert([
-      {
-        user_phone: phone,
-        memory_key: key,
-        memory_value: value
-      }
-    ])
-  console.log("Memory insert:", data)
-  console.log("Memory error:", error)
+  const { data: existingMemory } =
+    await supabase
+      .from("memories")
+      .select("*")
+      .eq("user_phone", phone)
+      .eq("memory_key", key)
+      .single()
+
+  if (existingMemory) {
+
+    const { error } =
+      await supabase
+        .from("memories")
+        .update({
+          memory_value: value
+        })
+        .eq("id", existingMemory.id)
+
+    console.log("Memory updated:", error)
+
+  } else {
+
+    const { error } =
+      await supabase
+        .from("memories")
+        .insert([
+          {
+            user_phone: phone,
+            memory_key: key,
+            memory_value: value
+          }
+        ])
+
+    console.log("Memory inserted:", error)
+  }
 }
 
 const getMemories = async (phone) => {
